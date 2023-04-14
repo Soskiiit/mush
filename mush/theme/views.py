c81722da4cd629388593565189664fc6a8c34d6f
Ts. Vadim <tsentsevitskiy.v@gmail.com>
2023-04-14 12:36:26 +0700

    Minor changes

--- mush/theme/views.py
+++ mush/theme/views.py
@@ -1,9 +1,33 @@
 from django.views.generic.base import TemplateView
 
 
+is_model_ready = 1
+is_user_the_owner = 1
+
+
 class ItemEditView(TemplateView):
     template_name = 'catalog/item-edit.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # For debug purposes only
+        local_context = {
+            'model_ready': is_model_ready,
+            'is_user_the_owner': is_user_the_owner,
+        }
+        context.update(local_context)
+        return context
 
 
 class ItemDetailsView(TemplateView):
     template_name = 'catalog/item-details.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # For debug purposes only
+        local_context = {
+            'model_ready': is_model_ready,
+            'is_user_the_owner': is_user_the_owner,
+        }
+        context.update(local_context)
+        return context
