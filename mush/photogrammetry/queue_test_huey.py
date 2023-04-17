016273df1e5cce5f971f23c5e909dc4b89ea3490
ulorew <kudashin.oleg@yandex.ru>
2023-04-17 13:33:59 +0300

    Fix small syntax

--- mush/photogrammetry/queue_test_huey.py
+++ mush/photogrammetry/queue_test_huey.py
@@ -3,15 +3,17 @@ from mush.settings import BASE_DIR
 
 # BASE_DIR = 'C:/Users/Oleg/PycharmProjects/mush/mush/'
 
-db_dir=f'{BASE_DIR}/db2.sqlite3'
+db_dir = f"{BASE_DIR}/db2.sqlite3"
 
-print(f'using database directory: {db_dir}')
+print(f"using database directory: {db_dir}")
 
 huey = SqliteHuey(filename=db_dir)
+
 
 @huey.task()
 def add(a, b):
     return a + b
+
 
 # В офф. доках предлагается в консоли сделать следующее
 # >>> from photogrammetry.queue_test_huey import add
@@ -19,4 +21,5 @@ def add(a, b):
 # >>> r()
 # И получить вывод 3
 
-# Можно запускать из shell, это также вынесено в файлик huey_test_runner, ниоткуда оно не работает
+# Можно запускать из shell, это также вынесено в файлик huey_test_runner,
+# ниоткуда оно не работает
